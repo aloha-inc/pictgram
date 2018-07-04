4151521d44da2081e7b5cea38a39adb4aedb33be
Nao <passmethepoke@gmail.com>
2018-07-04 17:22:39 +0900

    コード修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,11 @@ class UsersController < ApplicationController
   end
 
   def create
-  end  
+    @user = User.new(name: params[:user][:name], email: params[:user][:email])
+    if @user.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
 end
